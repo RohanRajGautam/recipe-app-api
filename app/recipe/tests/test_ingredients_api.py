0a2fbc61a9fb034d20c4eb32603a319e69711acc
rohan <rohanrajgautam@gmail.com>
2020-10-20 09:08:49 +0545

    implement features for list ingredients, and correction of test file

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -14,33 +14,33 @@ INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
 
 class PublicIngredientsApiTests(TestCase):
-    """Testt the publicly available ingredients api"""
+    """Test the publically available ingredients API"""
 
     def setUp(self):
         self.client = APIClient()
 
     def test_login_required(self):
-        """Test that login is required to access the endpoint"""
+        """Test that login is required to access this endpoint"""
         res = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class PrivateIngredientsApiTests(TestCase):
-    """Test the private ingredients API"""
+class PrivateIngredientsAPITests(TestCase):
+    """Test ingredients can be retrieved by authorized user"""
 
     def setUp(self):
         self.client = APIClient()
-        self.user = get_user_model().objects.create(
-            'rohan@rrg.com.np'
+        self.user = get_user_model().objects.create_user(
+            'test@londonappdev.com',
             'testpass'
         )
         self.client.force_authenticate(self.user)
 
-    def test_retrive_ingredient_list(self):
+    def test_retrieve_ingredient_list(self):
         """Test retrieving a list of ingredients"""
-        Ingredient.objects.create(user=self.user, name='Kale')
-        Ingredient.objects.create(user=self.user, name='Salt')
+        Ingredient.objects.create(user=self.user, name='kale')
+        Ingredient.objects.create(user=self.user, name='salt')
 
         res = self.client.get(INGREDIENTS_URL)
 
@@ -50,16 +50,26 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.data, serializer.data)
 
     def test_ingredients_limited_to_user(self):
-        """Test that the ingredients for the authenticated user are returned"""
+        """Test that only ingredients for authenticated user are returned"""
         user2 = get_user_model().objects.create_user(
-            'other@rrg.com.np',
+            'other@londonappdev.com',
             'testpass'
         )
         Ingredient.objects.create(user=user2, name='Vinegar')
-        ingredient = Ingredient.objects.create(user=self.user, name='Tumeric')
+
+        ingredient = Ingredient.objects.create(user=self.user, name='tumeric')
 
         res = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+
+
+
+
+
+
+
+
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 from recipe import serializers
 
@@ -24,3 +24,14 @@ class TagViewSet(viewsets.GenericViewSet,
         """Create a new tag"""
         serializer.save(user=self.request.user)
 
+
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage ingredient in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
