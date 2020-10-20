472ae338790a3d5282175619de00cf71bc906068
rohan <rohanrajgautam@gmail.com>
2020-10-20 09:34:13 +0545

    implemented features for creating ingredients

--- app/core/models.py
+++ app/core/models.py
@@ -53,7 +53,7 @@ class Ingredient(models.Model):
   name = models.CharField(max_length=255)
   user = models.ForeignKey(
       settings.AUTH_USER_MODEL,
-      on_delete=models.CASCADE
+      on_delete=models.CASCADE,
   )
 
   def __str__(self):
