202f9709ec8ab7c8f77e2856387189c66c792a15
rohan <rohanrajgautam@gmail.com>
2020-10-23 10:48:03 +0545

    implemented feature for filtering tags and ingredients

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Tag
+from core.models import Tag, Recipe
 
 from recipe.serializers import TagSerializer
 
@@ -82,48 +82,44 @@ class PrivateTagsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_retrieve_ingredients_assigned_to_recipes(self):
-        """Test for filtering ingredients by those assigned to recipes"""
-        ingredient1 = Ingredient.objects.create(
-            user=self.user, name='Apples'
-        )
-        ingredient2 = Ingredient.objects.create(
-            user=self.user, name='Turkey'
-        )
+    def test_retrieve_tags_assigned_to_recipes(self):
+        """Test filtering tags by those assigned to recipes"""
+        tag1 = Tag.objects.create(user=self.user, name='Breakfast')
+        tag2 = Tag.objects.create(user=self.user, name='Lunch')
         recipe = Recipe.objects.create(
-            title='Apple crumble',
-            time_minutes=5,
-            price=10.00,
-            user=self.user
+            title='Coriander eggs on toast',
+            time_minutes=10,
+            price=5.00,
+            user=self.user,
         )
-        recipe.ingredients.add(ingredient1)
+        recipe.tags.add(tag1)
 
-        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
 
-        serializer1 - IngredientSerializer(ingredient1)
-        serializer2 - IngredientSerializer(ingredient2)
+        serializer1 = TagSerializer(tag1)
+        serializer2 = TagSerializer(tag2)
         self.assertIn(serializer1.data, res.data)
         self.assertNotIn(serializer2.data, res.data)
 
-    def test_retrieve_ingredient_assigned_unique(self):
-        """Test filtering ingredients by assigned returns unique items"""
-        ingredient = Ingredient.objects.create(user=self.user, name='Eggs')
-        Ingredient.objects.create(user=self.user, name='Cheese')
+    def test_retrieve_tags_assigned_unique(self):
+        """Test filtering tags by assigned returns unique items"""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
+        Tag.objects.create(user=self.user, name='Lunch')
         recipe1 = Recipe.objects.create(
-            title='Eggs benedict',
-            time_minutes=30,
-            price=12.00,
+            title='Pancakes',
+            time_minutes=5,
+            price=3.00,
             user=self.user
         )
-        recipe1.ingredients.add(ingredient)
+        recipe1.tags.add(tag)
         recipe2 = Recipe.objects.create(
-            title='Green eggs on toast',
-            time_minutes=20,
-            price=5.00,
+            title='Porridge',
+            time_minutes=3,
+            price=2.00,
             user=self.user
         )
-        recipe2.ingredients.add(ingredient)
+        recipe2.tags.add(tag)
 
-        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
 
         self.assertEqual(len(res.data), 1)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -17,8 +17,17 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        """Return objects for current user"""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create a new object"""
